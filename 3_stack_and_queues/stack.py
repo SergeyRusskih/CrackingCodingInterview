6d5e108f18b182b93e0c72f9c9e7e6a8d0f50cc4
Sergei Russkikh <sergeivrusskikh@gmail.com>
2020-11-26 20:07:38 -0800

    stack implemented

--- 3_stack_and_queues/stack.py
+++ 3_stack_and_queues/stack.py
@@ -1,15 +1,62 @@
 class Stack:
     def __init__(self):
-        self.arr = []
+        self.tail = None
 
     def pop(self):
-        return self.arr.pop()
+        if not self.tail:
+            raise ValueError
+
+        current = self.tail
+        self.tail = current.Next
+        return current.data
 
     def peek(self):
-        return self.arr[len(self.arr)-1]
+        return self.tail.data
 
     def push(self, data):
-        self.arr.append(data)
+        node = Node(data)
+        if not self.tail:
+            self.tail = node
+        else:
+            node.Next = self.tail
+            self.tail = node
 
-    def isEmpty(self):
-        return len(self.arr) == 0
+    def is_empty(self):
+        return not self.tail
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.Next = None
+
+
+def test_is_not_empty():
+    stack = Stack()
+    stack.push(1)
+    stack.push(2)
+
+    assert not stack.is_empty()
+
+def test_is_empty():
+    stack = Stack()
+    stack.push(1)
+    stack.push(3)
+
+    stack.pop()
+    stack.pop()
+
+    assert stack.is_empty()
+
+def test_peek():
+    stack = Stack()
+    stack.push(4)
+
+    assert stack.peek() == 4
+    assert not stack.is_empty()
+
+def test_pop():
+    stack = Stack()
+    stack.push(4)
+
+    assert stack.pop() == 4
+    assert stack.is_empty()
