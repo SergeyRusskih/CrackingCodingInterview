e3a39c5a03b110c4c78e14c68d22facb34296a1d
srrusski <srrusski@amazon.com>
2020-12-01 09:21:14 -0800

    list of depths solved

--- 4_trees_and_graphs/4_list_of_depths.py
+++ 4_trees_and_graphs/4_list_of_depths.py
@@ -3,6 +3,12 @@ class Node:
         self.data = data
         self.next = None
 
+    def append(self, node):
+        next = self
+        while next.next:
+            next = next.next
+        next.next = node
+
 def list_of_depths(tree, index):
     nodes = []
     nodes.append(Node(tree[index-1]))
@@ -15,23 +21,24 @@ def list_of_depths(tree, index):
         node = None
         for _, i in enumerate(indexes):
             left = get_left(i)
-            if left < len(tree):
+            if left <= len(tree):
                 children.append(left)
                 if not node:
-                    node = = Node(tree[left-1])
+                    node = Node(tree[left-1])
                 else:
-                    node.next = Node(tree[left-1])
+                    node.append(Node(tree[left-1]))
                 
 
             right = get_right(i)
-            if right < len(tree):
+            if right <= len(tree):
                 children.append(right)
                 if not node:
                     node = Node(tree[right-1])
                 else:
-                    node.next = Node(tree[right-1])
+                    node.append(Node(tree[right-1]))                    
 
-        nodes.append(node.next)
+        if node:
+            nodes.append(node)
         indexes = children
 
     return nodes
