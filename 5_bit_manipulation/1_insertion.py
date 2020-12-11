dca5fe43ee28abdf258443bd3264ad50c0a1510b
Sergei Russkikh <sergeivrusskikh@gmail.com>
2020-12-10 21:01:18 -0800

    insertion

--- 5_bit_manipulation/1_insertion.py
+++ 5_bit_manipulation/1_insertion.py
@@ -1,7 +1,10 @@
-def insertion(n, m, j, i):
-    pass
+def insertion(n, m, i, j):
+    mask = ((1 << 33) - 1 << j+1) | (1 << i) - 1
+    return bin((n & mask) | (m << i))
 
 def test_insertion():
-    n = 0b10000000000
+    n = 0b10000100001
     m = 0b10011
-    assert insertion(n, m) == 0b10001001100
+    result = insertion(n, m, 2, 6)
+    assert result == bin(0b10001001101)
+    
