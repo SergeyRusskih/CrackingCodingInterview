09ddbe783cb7f90688558117b9cebb7b2f33916b
Sergei Russkikh <sergeivrusskikh@gmail.com>
2020-12-12 14:56:34 -0800

    next number

--- 5_bit_manipulation/4_next_number.py
+++ 5_bit_manipulation/4_next_number.py
@@ -1,9 +1,29 @@
 def next_number(n):
     index = len(n) - 1
-    arr = n.split()
-    while arr[index] != b:
-        pass
+    arr = list(n)
 
+    smalest = None
+    largest = None
+    while arr[index] != 'b':
+        if arr[index] == '0':
+            if not smalest and index != len(n) - 1 and arr[index-1] == '1':
+                smalest = swap(arr, index, index-1)
+            if not largest and arr[index+1] == '1':
+                largest = swap(arr, index, index+1)
+
+        if smalest and largest:
+            return smalest, largest
+
+        index -= 1
+
+    return smalest, largest
+
+def swap(input, index1, index2):
+    arr = input.copy()
+    tmp = arr[index1]
+    arr[index1] = arr[index2]
+    arr[index2] = tmp
+    return "".join(arr)
 
 def test_simple():
     smalest, largest = next_number('0b001011011')
